14217c89ee71119cbefe3287ae92c800b5433ec4
{Bright-ship-it} <{wilbright.amugune@student.moringaschool.com}>
2022-09-12 22:37:03 +0300

    rendering

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+
+    def index
+        render json: Comments.all
+    end
+
+    def create
+        byebug
+        comment = Comment.new(comment_params)
+
+        if comment.save
+            render json: comment
+        else
+            byebug 
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content)
+    end
 end
